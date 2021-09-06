3edadd041dfef3f784fec91ae5de4eccb403986c
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-09-06 13:51:44 +0200

    initial commit

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void MoveForward()
+    {
+        Debug.Log("hey");
+    }
+}
