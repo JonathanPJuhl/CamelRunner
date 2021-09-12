1fb0ac0c0d55021f283c5527c00f6cec400d823c
Cathrine <cph-ac221@cphbusiness.dk>
2021-09-12 21:55:18 +0200

    Player can jump now

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -12,6 +12,9 @@ public class PlayerMovementController : MonoBehaviour
     public float speedIncreasePerPoint = 0.1f;
     bool isAlive = true;
 
+    [SerializeField] float jumpForce = 400f;
+    [SerializeField] LayerMask groundMask;
+ 
     private void FixedUpdate()
     {
         if (!isAlive) return;
@@ -23,6 +26,11 @@ public class PlayerMovementController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
 
         if (transform.position.y < -5)
         {
@@ -40,5 +48,15 @@ public class PlayerMovementController : MonoBehaviour
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Jump()
+    {
+        //Check wether we are currently grounded
+        float height = GetComponent<Collider>().bounds.size.y;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, (height/2) + 0.01f, groundMask);
+        
+        //If we are on the ground, jump 
+        rb.AddForce(Vector3.up * jumpForce);
     }
 }
